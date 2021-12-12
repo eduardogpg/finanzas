8b03b308824eb8d63cf7da44f610dde9938f1049
eduardogpg <eduardo78d@gmail.com>
2021-12-12 15:41:08 -0600

    Mostrar ultimos creditos

--- finanzas/finanzas/views.py
+++ finanzas/finanzas/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 
+from prospects.models import Client
+
 def dashboard(request):
-    context = {}
+    context = {
+        'title' :'Finanzas del sur',
+        'clients': Client.objects.all()
+    }
     return render(request, 'index.html', context)
 
 def index(request):
